2c4aba9f609ea7de33c3390b2f01a28b64a1a917
Renato Silva <orenatodos@gmail.com>
2023-01-24 14:48:34 -0300

    fix: freshdesk widget

--- src/pages/finalizar-atendimento.tsx
+++ src/pages/finalizar-atendimento.tsx
@@ -12,24 +12,6 @@ export default function FinalizarAtendimento() {
         <title>Finalizar atendimento – Siga Antenado</title>
       </Head>
 
-      <Script id="freshchat" type="text/javascript">
-        {`
-        function initFreshChat() {
-          window.fcWidget.init({
-            token: "71760711-ffb7-4517-891b-bc33d9854c0f",
-            host: "https://sigaantenado.freshchat.com",
-            open: true
-          });
-        }
-        function initialize(i,t){var e;i.getElementById(t)?
-        initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,
-        e.src="https://sigaantenado.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}
-        function initiateCall(){initialize(document,"Freshchat-js-sdk")}
-        window.addEventListener?window.addEventListener("load",initiateCall,!1):
-        window.attachEvent("load",initiateCall,!1);
-        `}
-      </Script>
-
       <Box minH="100vh" py="10" px="6" bg="blackAlpha.50">
         <Box maxW={800} margin="0 auto" p={8} borderRadius={10} bg="white">
           <FormHeader />
@@ -37,6 +19,31 @@ export default function FinalizarAtendimento() {
           <Success />
         </Box>
       </Box>
+
+      <Script
+        id="freshchat"
+        type="text/javascript"
+        async
+        defer
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function initFreshChat() {
+              window.fcWidget.init({
+                token: "71760711-ffb7-4517-891b-bc33d9854c0f",
+                host: "https://sigaantenado.freshchat.com",
+                open: true
+              });
+            }
+            function initialize(i,t){var e;i.getElementById(t)?
+            initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,
+            e.src="https://sigaantenado.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}
+            function initiateCall(){initialize(document,"Freshchat-js-sdk")}
+            window.addEventListener?window.addEventListener("load",initiateCall,!1):
+            window.attachEvent("load",initiateCall,!1);
+          `
+        }}
+      />
     </>
   )
 }
