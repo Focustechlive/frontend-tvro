036e0994ce5532a69d423eb624bce45c6f704b34
Renato Silva <orenatodos@gmail.com>
2023-01-24 15:24:11 -0300

    fix: freshdesk widget

--- src/pages/finalizar-atendimento.tsx
+++ src/pages/finalizar-atendimento.tsx
@@ -12,12 +12,6 @@ export default function FinalizarAtendimento() {
         <title>Finalizar atendimento – Siga Antenado</title>
       </Head>
 
-      <Script id="freshchat" type="text/javascript">
-        {`
-         (function (d, w, c) { if(!d.getElementById("spd-busns-spt")) { var n = d.getElementsByTagName('script')[0], s = d.createElement('script'); var loaded = false; s.id = "spd-busns-spt"; s.async = "async"; s.setAttribute("data-self-init", "false"); s.setAttribute("data-init-type", "opt"); s.src = 'https://cdn.freshbots.ai/assets/share/js/freshbots.min.js'; s.setAttribute("data-client", "8d5d03264074648c2469dee6168f444bf5602a0f"); s.setAttribute("data-bot-hash", "9f5e967001b19386d260722806c27b1d88a6f70f"); s.setAttribute("data-env", "prod"); s.setAttribute("data-region", "us"); if (c) { s.onreadystatechange = s.onload = function () { if (!loaded) { c(); } loaded = true; }; } n.parentNode.insertBefore(s, n); } }) (document, window, function () { Freshbots.initiateWidget({ autoInitChat: false, getClientParams: function () { return ; } }, function(successResponse) { }, function(errorResponse) { }); });
-        `}
-      </Script>
-
       <Box minH="100vh" py="10" px="6" bg="blackAlpha.50">
         <Box maxW={800} margin="0 auto" p={8} borderRadius={10} bg="white">
           <FormHeader />
@@ -25,6 +19,32 @@ export default function FinalizarAtendimento() {
           <Success />
         </Box>
       </Box>
+
+      <Script
+        id="freshchat"
+        type="text/javascript"
+        async
+        defer
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function initFreshChat() {
+              window.fcWidget.init({
+                token: "71760711-ffb7-4517-891b-bc33d9854c0f",
+                host: "https://sigaantenado.freshchat.com",
+                widgetUuid: "d20b9e53-5c4c-48c8-847a-162082423865",
+                open: true
+              });
+            }
+            function initialize(i,t){var e;i.getElementById(t)?
+            initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,
+            e.src="https://sigaantenado.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}
+            function initiateCall(){initialize(document,"Freshchat-js-sdk")}
+            window.addEventListener?window.addEventListener("load",initiateCall,!1):
+            window.attachEvent("load",initiateCall,!1);
+          `
+        }}
+      />
     </>
   )
 }
