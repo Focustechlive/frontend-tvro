e2317562b152c2b7e661a22518e8d59f1a268d67
marcosmachado <miyanishi.dm@gmail.com>
2023-02-14 15:30:49 -0300

    Alterando a busca de cep e IBGE

--- src/pages/api/v1/cep2/[cep2].ts
+++ src/pages/api/v1/cep2/[cep2].ts
@@ -8,35 +8,24 @@ export default async function handler(
   let token: any
   try {
     const API_URL_Token = process.env.DATASINTESE_URL_TOKEN as string
-    var API_URL = process.env.DATASINTESE_URL as string
     const params = new URLSearchParams()
     params.append('username', process.env.DATASINTESE_USERNAME as string)
     params.append('password', process.env.DATASINTESE_PASSWORD as string)
     params.append('expires_in', '84600')
     params.append('grant_type', 'password')
-    const response = await axios.post(
-      API_URL_Token,
-      params,
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }
-    )
+    const response = await axios.post(API_URL_Token, params, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })
     token = 'Bearer ' + response.data.access_token
   } catch {
     return res.status(500).end('Internal server error')
   }
 
   try {
-    const response1 = await axios.get(
-      `${API_URL} ${req.query.cep2}`,
-      {
-        headers: {
-          Authorization: `${token}`
-        }
-      }
-    )
+    const API_URL = process.env.DATASINTESE_URL as string
+    const response1 = await axios.get(`${API_URL} ${req.query.cep2}`, {
+      headers: { Authorization: `${token}` }
+    })
 
     const { cep, bairro, localidade, logradouro, uf, ibge } = {
       cep: response1.data.sintese.endereco_postal['0'].cep,
